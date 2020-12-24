01571dcfdc58f974fe84d50d09cf12a2f82ed152
justeel <76513261+justeel@users.noreply.github.com>
2020-12-24 18:33:53 +0900

    Update helloworld.py

--- helloworld.py
+++ helloworld.py
@@ -1,34 +1,25 @@
-# 슬라이싱
-joochul = "971211-1234567"
+print("필생즉사 \n필사즉생")
 
-print("성별 : " + joochul[7])
-print("연 : " + joochul[:2])
-print("생년월일 :" + joochul[:6])
 
-print("뒤 7자리" + joochul[7:])
-print("뒤 7자리 (뒤부터)" +joochul[-7:])
+# 저는 "김주철" 입니다 .
+# print("저는 \"김주철\" 입니다.")
 
-# 문자열 처리함수
-python = " naver "
-print(python.lower())
-print(python.upper())
-print(python.replace("naver","google"))
+# \r : 커서를 맨 앞으로 이동 
 
-index = python.index("n")
-print(index)
-print(python.find("google"))
-#문자열 포맷
-# print("나는 %d살 입니다" % 24) 
-# print("나는 %s을 좋아해요"  % "파이썬")
-# print("Apple 은 %c로 시작해요" % "A")
-print("나는 %s색과 %s색을 좋아해요." % ("파란", "빨강"))
-print("나는 {}살 입니다" .format(24))
-print("나는 {1}색과 {0}색을 좋아해요.)" .format("빨간", "파란"))
-print("나는 {a}살이며, {c}색을 좋아해요." .format(a = 24, c = "분홍"))
 
-a = 24
-c = "분홍"
-print(f"나는 {a}살이며, {c}색을 좋아해요.")
+print("red apple\rpine")
 
-# 줄바꿈
-print("필사즉생 \n필생즉사 ")
+# \b : 백스페이스 (한 글자 삭제)
+print("redd\bAplle")
+
+#\t : 탭
+print("red \t apple5")
+
+url = "http://naver.com"
+my_str = url.replace("http://", "") # replace = 바꾸는거
+print(my_str)
+my_str = my_str[:my_str.index(".")] 
+#index . < . 전까지만 출력 0~ 5 직전까지
+print(my_str)
+password = my_str[:3] + str(len(my_str)) + str(my_str.count("e")) + "!"
+print("{0} 의 비밀 번호는 {1} 입니다 ". format(url, password))
