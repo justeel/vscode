bea04aa36b873744e7af9f49ad4a38ef100123c2
justeel <76513261+justeel@users.noreply.github.com>
2020-12-30 20:51:44 +0900

    if and for

---
+++ if.py
@@ -0,0 +1,44 @@
+# weather = input("오늘 날씨는 어때요?")
+# if weather == "비" or weather == "눈":
+#     print("우산을 챙기세요")
+# elif weather == "미세먼지":
+#     print("마스크를 챙기세요")
+# else:
+#     print("준비물 필요 없어요")
+
+# temp = int(input("기온은 어때요?"))
+# if 30 <= temp: # 30도 이상
+#     print("너무 더워요 나가지 마세요")
+# elif 10 <= temp and temp < 30: # 10도이상 그리고 30도 미만이면 
+#     print("괜찮은 날씨에요")
+# elif 0 <= temp and temp <
+#     print("외투를 챙기세요")
+# else:
+#     print("그냥 집에있을래")
+
+#if
+#숫자
+# b = int(input("얼마있어?"))
+# if 100000 <= b:
+#     print("너 부자구나?")
+# elif 10000 <= b and b < 100000:
+#     print(" 놀러가자")
+# else:
+#     print("너 그지구나?")    
+
+#for
+# print("대기번호 : 1")
+# print("대기번호 : 2")
+# print("대기번호 : 3")
+# for waitng in range(1,100):
+#     print("대기번호 :  %s " % (waitng))
+
+s = ["아이언맨", "토르", "김주철"]
+for a in s: #스타벅스 안에있는 사람한명씩 부르면서 출력
+    print("%s, 커피가 준비되었습니다" %(a))
+
+# for i in range(1, 100):
+#     if i % 2 == 0:
+#         print("짝수")
+#     else:
+#         print(i)
